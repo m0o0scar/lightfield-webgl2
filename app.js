e5277901cf61b9852225400dccd30963a7858748
Oscar <tangqh@sea.com>
2022-12-19 10:41:09 +0800

    refactor: keep only focus option; replace with nerf output video

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 import * as THREE from './vendor/three.module.js';
-import { OrbitControls } from './vendor/OrbitControls.js';
 
-const apertureInput = document.querySelector('#aperture');
 const focusInput = document.querySelector('#focus');
-const stInput = document.querySelector('#stplane');
-const loadWrap = document.querySelector('#load-wrap');
-const loadBtn = document.querySelector('#load');
 
 const scene = new THREE.Scene();
 let width = window.innerWidth;
 let height = window.innerHeight;
-const camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100);
 const renderer = new THREE.WebGLRenderer();
 let fragmentShader, vertexShader;
 
@@ -22,56 +17,30 @@ scene.add(camera);
 let fieldTexture;
 let plane, planeMat, planePts;
 const filename = './frames.mp4';
-const camsX = 17;
-const camsY = 17;
-const resX = 1024;
-const resY = 1024;
-const cameraGap = 0.08; // cm hardcoded for now
-let aperture = Number(apertureInput.value);
+const camsX = 48;
+const camsY = 1;
+const resX = 960;
+const resY = 1280;
 let focus = Number(focusInput.value);
-
-// const helper = new THREE.AxesHelper(0.1);
-// scene.add(helper);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target = new THREE.Vector3(0,0,1);
-controls.panSpeed = 2;
 
 window.addEventListener('resize', () => {
   width = window.innerWidth;
   height = window.innerHeight;
-  camera.aspect = width/height;
+  camera.aspect = width / height;
   camera.updateProjectionMatrix();
   renderer.setSize(width, height);
   renderer.render(scene, camera);
 });
 
-apertureInput.addEventListener('input', e => {
-  aperture = Number(apertureInput.value);
-  planeMat.uniforms.aperture.value = aperture;
-});
-
 focusInput.addEventListener('input', e => {
   focus = Number(focusInput.value);
+  console.log('focus', focus);
   planeMat.uniforms.focus.value = focus;
 });
 
-stInput.addEventListener('input', () => {
-  planePts.visible = stInput.checked;
-});
-
-loadBtn.addEventListener('click', async () => {
-  loadBtn.setAttribute('disabled', true);
-  await loadScene();
-});
-
-
-
 function animate() {
-	requestAnimationFrame(animate);
-  controls.update();
-	renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 async function loadScene() {
@@ -104,17 +73,17 @@ async function extractVideo() {
   console.log('starting extraction');
 
   const getBufferFromVideo = () => {
-    ctx.drawImage(video, 0, 0,resX, resY);
-    const imgData = ctx.getImageData(0,0,resX, resY);
+    ctx.drawImage(video, 0, 0, resX, resY);
+    const imgData = ctx.getImageData(0, 0, resX, resY);
     allBuffer.set(imgData.data, offset);
     offset += imgData.data.byteLength;
     count++;
-    loadBtn.textContent = `Loaded ${Math.round(100 * count / (camsX * camsY))}%`;
+    console.log(`Loaded ${Math.round(100 * count / (camsX * camsY))}%`);
   };
 
   const fetchFrames = async () => {
     let currentTime = 0;
-    
+
     while (count < camsX * camsY) {
       getBufferFromVideo();
       currentTime += 0.0333;
@@ -122,8 +91,6 @@ async function extractVideo() {
       await new Promise(res => (seekResolve = res));
     }
 
-    loadWrap.style.display = 'none';
-    
     fieldTexture = new THREE.DataTexture2DArray(allBuffer, resX, resY, camsX * camsY);
     console.log('Loaded field data');
 
@@ -131,8 +98,8 @@ async function extractVideo() {
     fieldTexture.needsUpdate = true;
   };
 
-  video.addEventListener('seeked', async function() {
-    if(seekResolve) seekResolve();
+  video.addEventListener('seeked', async function () {
+    if (seekResolve) seekResolve();
   });
 
 
@@ -143,22 +110,20 @@ async function extractVideo() {
 }
 
 function loadPlane() {
-  const planeGeo = new THREE.PlaneGeometry(camsX * cameraGap, camsY * cameraGap, camsX, camsY);
+  const planeGeo = new THREE.PlaneGeometry(resX, resY, camsX, camsY);
   planeMat = new THREE.ShaderMaterial({
     uniforms: {
       field: { value: fieldTexture },
       camArraySize: new THREE.Uniform(new THREE.Vector2(camsX, camsY)),
-      aperture: { value: aperture },
+      aperture: { value: 5 },
       focus: { value: focus }
     },
     vertexShader,
     fragmentShader,
   });
   plane = new THREE.Mesh(planeGeo, planeMat);
-  const ptsMat = new THREE.PointsMaterial({ size: 0.01, color: 0xeeccff });
-  planePts = new THREE.Points(planeGeo, ptsMat);
-  planePts.visible = stInput.checked;
-  plane.add(planePts);
   scene.add(plane);
   console.log('Loaded plane');
 }
+
+loadScene();
